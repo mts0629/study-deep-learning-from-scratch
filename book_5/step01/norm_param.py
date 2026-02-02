9288e33b0c69c987c544c5c2206b0c9cdee951b6
mts0629 <soratobi93@gmail.com>
2026-02-02 23:47:39 +0900

    Book #5: step 1, normal distribution

--- book_5/step01/norm_param.py
+++ book_5/step01/norm_param.py
@@ -1,15 +1,46 @@
 import numpy as np
 
 def normal(x, mu=0, sigma=1):
-    y = 1 / (np.sqrt(2 * np.pi) * sigma) * np.exp(-(x - mu)**2 / 2 * sigma**2)
+    y = 1 / (np.sqrt(2 * np.pi) * sigma) * np.exp(-(x - mu)**2 / (2 * sigma**2))
     return y
 
 import matplotlib.pyplot as plt
 
 x = np.linspace(-5, 5, 100)
-y = normal(x)
 
+# Normal distribution
+y = normal(x)
 plt.plot(x, y)
 plt.xlabel("x")
 plt.ylabel("y")
 plt.savefig("./normal.png")
+
+plt.cla()
+
+x = np.linspace(-10, 10, 1000)
+
+# Change mean
+y0 = normal(x, mu=3)
+y1 = normal(x, mu=0)
+y2 = normal(x, mu=-5)
+plt.plot(x, y0, label="$\mu$=3")
+plt.plot(x, y1, label="$\mu$=0")
+plt.plot(x, y2, label="$\mu$=-5")
+plt.legend()
+plt.xlabel("x")
+plt.xlabel("y")
+plt.savefig("./mean.png")
+
+plt.cla()
+
+# Change std. dev
+y0 = normal(x, mu=0, sigma=0.5)
+y1 = normal(x, mu=0, sigma=1)
+y2 = normal(x, mu=0, sigma=2)
+plt.plot(x, y0, label="$\sigma$=0.5")
+plt.plot(x, y1, label="$\sigma$=1")
+plt.plot(x, y2, label="$\sigma$=2")
+plt.legend()
+plt.xlabel("x")
+plt.xlabel("y")
+plt.savefig("./stddev.png")
