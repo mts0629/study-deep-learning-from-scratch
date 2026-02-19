4a63eb37bc250b2e9f42d97d3c0c915925390f09
mts0629 <soratobi93@gmail.com>
2026-02-19 23:52:13 +0900

    Book #5: step 7, encoder and decoder

---
+++ book_5/step07/vae.py
@@ -0,0 +1,46 @@
+import matplotlib.pyplot as plt
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import torchvision
+from torchvision import datasets, transforms
+
+
+# Hyperparameters
+input_dim = 784  # Input size (28x28)
+hidden_dim = 200  # Dimension of hidden layer
+latent_dim = 20  # Dimension of latent vector
+epochs = 30
+learning_rate = 3e-4
+batch_size = 32
+
+
+class Encoder(nn.Module):
+    def __init__(self, input_dim, hidden_dim, latent_dim):
+        super().__init__()
+        self.linear = nn.Linear(input_dim, hidden_dim)
+        self.linear_mu = nn.Linear(hidden_dim, latent_dim)
+        self.linear_logvar = nn.Linear(hidden_dim, latent_dim)
+
+    def forward(self, x):
+        h = self.linear(x)
+        h = F.relu(h)
+        mu = self.linear_mu(h)
+        logvar = self.linear_logvar(h)
+        sigma = torch.exp(0.5 * logvar)
+        return mu, sigma
+
+
+class Decoder(nn.Module):
+    def __init__(self, latent_dim, hidden_dim, output_dim):
+        super().__init__()
+        self.linear1 = nn.Linear(latent_dim, hidden_dim)
+        self.linear2 = nn.Linear(hidden_dim, output_dim)
+
+    def forward(self, z):
+        h = self.linear1(z)
+        h = F.relu(h)
+        h = self.linear2(h)
+        x_hat = F.sigmoid(h)
+        return x_hat
