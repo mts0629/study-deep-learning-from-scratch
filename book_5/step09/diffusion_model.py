1b2a68f5c8ab93f1e0901e7eb56f8efdf22462af
mts0629 <soratobi93@gmail.com>
2026-04-14 23:32:36 +0900

    Book #5: step 9, UNet with positional encoding

--- book_5/step09/diffusion_model.py
+++ book_5/step09/diffusion_model.py
@@ -1,4 +1,5 @@
 import torch
+from torch import nn
 
 
 # Sinusoidal positional encoding
@@ -26,6 +27,67 @@ def pos_encoding(ts, output_dim, device="cpu"):
     return v
 
 
-if __name__ == "__main__":
-    v = pos_encoding([1, 2, 3], 16)
-    print(v.shape)
+class ConvBlock(nn.Module):
+    def __init__(self, in_ch, out_ch, time_embed_dim):
+        super().__init__()
+        self.convs = nn.Sequential(
+            nn.Conv2d(in_ch, out_ch, 3, padding=1),
+            nn.BatchNorm2d(out_ch),
+            nn.ReLU(),
+            nn.Conv2d(out_ch, out_ch, 3, padding=1),
+            nn.BatchNorm2d(out_ch),
+            nn.ReLU()
+        )
+        # MLP
+        self.mlp = nn.Sequential(
+            nn.Linear(time_embed_dim, in_ch),
+            nn.ReLU(),
+            nn.Linear(in_ch, in_ch)
+        )
+
+    def forward(self, x, v):
+        N, C, _, _ = x.shape
+        v = self.mlp(v)
+        v = v.view(N, C, 1, 1)
+        y = self.convs(x + v)
+        return y
+
+
+class UNet(nn.Module):
+    def __init__(self, in_ch=1, time_embed_dim=100):
+        super().__init__()
+        self.time_embed_dim = time_embed_dim
+
+        self.down1 = ConvBlock(in_ch, 64, time_embed_dim)
+        self.down2 = ConvBlock(64, 128, time_embed_dim)
+        self.bot1 = ConvBlock(128, 256, time_embed_dim)
+        self.up2 = ConvBlock(128 + 256, 128, time_embed_dim)
+        self.up1 = ConvBlock(128 + 64, 64, time_embed_dim)
+        self.out = nn.Conv2d(64, in_ch, 1)
+
+        self.maxpool = nn.MaxPool2d(2)
+        self.upsample = nn.Upsample(
+            scale_factor=2, mode="bilinear"
+        )
+
+    def forward(self, x, timesteps):
+        # Sinusoidal positional encoding
+        v = pos_encoding(
+            timesteps, self.time_embed_dim, x.device
+        )
+
+        x1 = self.down1(x, v)
+        x = self.maxpool(x1)
+        x2 = self.down2(x, v)
+        x = self.maxpool(x2)
+
+        x = self.bot1(x, v)
+
+        x = self.upsample(x)
+        x = torch.cat([x ,x2], dim=1)
+        x = self.up2(x, v)
+        x = self.upsample(x)
+        x = torch.cat([x, x1], dim=1)
+        x = self.up1(x, v)
+        x = self.out(x)
+        return x
