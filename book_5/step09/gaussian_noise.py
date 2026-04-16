6b5192fda9908f6d3a07154b29b90cc37c929e5f
mts0629 <soratobi93@gmail.com>
2026-04-16 23:54:03 +0900

    Book #5: step 9, sampling noise

--- book_5/step09/gaussian_noise.py
+++ book_5/step09/gaussian_noise.py
@@ -52,3 +52,34 @@ for i, img in enumerate(imgs[:10]):
     plt.title(f"Noise: {i * 100}")
     plt.axis("off")
 plt.savefig("./gaussian_noise.png")
+
+plt.clf()
+
+
+# Add gaussian noise at time t to the image
+def add_noise(x_0, t, betas):
+    T = len(betas)
+    assert t >= 1 and t <= T
+
+    alphas = 1 - betas
+    alpha_bars = torch.cumprod(alphas, dim=0)  # Cumulative product
+    t_idx = t - 1
+    alpha_bar = alpha_bars[t_idx]
+
+    eps = torch.randn_like(x_0)
+    x_t = torch.sqrt(alpha_bar) * x_0 + torch.sqrt(1 - alpha_bar) * eps
+
+    return x_t
+
+
+image = plt.imread(file_path)
+x = preprocess(image)
+
+t = 100
+x_t = add_noise(x, t, betas)
+
+img = reverse_to_img(x_t)
+plt.imshow(img)
+plt.title(f"Noise: t={t}")
+plt.axis("off")
+plt.savefig("./noise_t100.png")
